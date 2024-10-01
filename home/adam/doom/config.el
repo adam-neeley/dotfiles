fd67b7f7325ead3c798bb80c3ccf7d8af82423ec
monadam <aneeley@gmail.com>
2024-10-01 16:47:41 -0700

    org stuff

--- home/adam/doom/config.el
+++ home/adam/doom/config.el
@@ -51,16 +51,39 @@
 (setq-default explicit-shell-file-name (executable-find "fish"))
 
 ;; org-mode
-(setq! org-directory    "~/docs/org/"
+(setq! org-directory "~/docs/org/"
        org-agenda-files (apply 'append
                                (mapcar
                                 (lambda (directory)
                                   (directory-files-recursively directory org-agenda-file-regexp))
                                 '("~/docs/org/" "~/school/" "~/dotfiles/")))
        org-startup-with-latex-preview 't
-       org-todo-keywords '((sequence "TODO(t)" "NEXT(n)" "PROG(p)" "INTR(i)" "DONE(d)"))
+       ;; org-todo-keywords '((sequence "TODO(t)" "NEXT(n)" "PROG(p)" "INTR(i)" "DONE(d)"))
        org-agenda-with-colors 't
        org-hide-macro-markers 't
+       org-capture-templates '(("t" "Personal todo" entry
+                                (file+headline +org-capture-todo-file "Inbox")
+                                "* [ ] %?\n%i\n" :prepend t)
+                               ("n" "Personal notes" entry
+                                (file+headline +org-capture-notes-file "Inbox")
+                                "* %u %?\n%i\n" :prepend t)
+                               ("j" "Journal" entry
+                                (file+olp+datetree +org-capture-journal-file)
+                                "* %U %?\n%i\n" :prepend t)
+                               ("p" "Templates for projects")
+                               ("pt" "Project-local todo" entry
+                                (file+headline +org-capture-project-todo-file "Inbox")
+                                "* TODO %?\n%i\n" :prepend t)
+                               ("pn" "Project-local notes" entry
+                                (file+headline +org-capture-project-notes-file "Inbox")
+                                "* %U %?\n%i\n" :prepend t)
+                               ("pc" "Project-local changelog" entry
+                                (file+headline +org-capture-project-changelog-file "Unreleased")
+                                "* %U %?\n%i\n" :prepend t)
+                               ("o" "Centralized templates for projects")
+                               ("ot" "Project todo" entry #'+org-capture-central-project-todo-file "* TODO %?\n %i\n" :heading "Tasks" :prepend nil)
+                               ("on" "Project notes" entry #'+org-capture-central-project-notes-file "* %U %?\n %i\n" :heading "Notes" :prepend t)
+                               ("oc" "Project changelog" entry #'+org-capture-central-project-changelog-file "* %U %?\n %i\n" :heading "Changelog" :prepend t))
        org-hide-emphasis-markers 't)
 
 (defvar my-org-hidden-keywords
@@ -113,7 +136,7 @@
 (setq!
  projectile-auto-discover 't
  projectile-sort-order 'recently-active
- projectile-project-search-path '("/home/adam/code/" "/home/adam/school/")
+ projectile-project-search-path '("~/docs/" "~/code/" "~/school/")
  savehist-autosave-interval 10)
 
 ;; auto-load
