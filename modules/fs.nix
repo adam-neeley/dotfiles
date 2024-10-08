42344596f235a8fe51acda8d0658c5ff2734c9c2
monadam <aneeley@gmail.com>
2024-10-07 21:36:39 -0700

    davfs2/webDAV stuff

--- modules/fs.nix
+++ modules/fs.nix
@@ -1,14 +1,14 @@
 { config, lib, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ davfs2 ];
+  services.davfs2.enable = true;
 
   fileSystems = {
-    "/home/adam/docs/org/davfs/" = {
-      device = "http://tornado:4918/public/";
+    "/home/adam/docs/org/davfs" = {
+      device = "http://tornado:4918/user/monadam/";
       fsType = "davfs";
-      options = [ "nofail" ];
+      # options = [ "rw,nofail,user" ];
+      options = [ "user,noauto,file_mode=600,dir_mode=700 0 1" ];
     };
   };
-
 }
