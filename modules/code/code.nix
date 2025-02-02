b4088fc771b261889a68678925516ce0c161741e
Adam Neeley <adam.t.neeley@protonmail.com>
2025-02-01 16:07:06 -0800

    add phaser-editor & electron libraries

--- modules/code/code.nix
+++ modules/code/code.nix
@@ -12,6 +12,39 @@ in {
       programs.nix-ld = {
         enable = true; # run unpatched binaries #
         package = pkgs.nix-ld-rs;
+        libraries = with pkgs;[
+          # the following is to run Phaser-Editor
+          glib
+          nss
+          nspr
+          dbus
+          at-spi2-atk
+          cups
+          libdrm
+          gtk3
+          pango
+          cairo
+          xorg.libX11
+          xorg.libXcomposite
+          xorg.libXcursor
+          xorg.libXdamage
+          xorg.libXext
+          xorg.libXi
+          xorg.libXfixes
+          xorg.libXinerama
+          xorg.libXrandr
+          xorg.libXrender
+          xorg.libXtst
+          xorg.libXt
+          mesa
+          expat
+          xorg.libxcb
+          libxkbcommon
+          alsa-lib
+          # electron-builder
+          libGL
+
+        ];
       };
 
       programs.git = {
